ac96255f99a9769a6d0f016af9f8daa858587ff3
Orwiss <pjtpjt159@naver.com>
2021-03-04 10:02:14 +0900

    p5.js study Day2

--- 2/example.js
+++ 2/example.js
@@ -4,26 +4,29 @@ let r = 0;
 let scale = 50;
 
 function setup() {
-  createCanvas(displayWidth, displayHeight);
-  noFill();
-  stroke(255);
-  strokeWeight(4);
+    var canvas = createCanvas(document.body.clientWidth, 720);
+    canvas.parent('header-motion');
+    noFill();
+    stroke(255);
+    strokeWeight(4);
 }
 
 function draw() {
-  background(0);
+    background(0);
+    stroke(255);
+    circle(mouseX, mouseY, 50);
+    stroke(255, 0, 0);
+    circle(mouseX + (cos(radians(deg)) * r), mouseY + (sin(radians(deg)) * r), scale);
+    stroke(0, 0, 255);
+    circle(mouseX + (cos(radians(deg+180)) * r), mouseY + (sin(radians(deg+180)) * r), scale);
 
-  circle(mouseX, mouseY, 50);
-  circle(mouseX + (cos(radians(deg)) * r), mouseY + (sin(radians(deg)) * r), scale);
-  circle(mouseX + (cos(radians(deg+180)) * r), mouseY + (sin(radians(deg+180)) * r), scale);
-
-  deg ++;
-  r += 1 * pm;
-  scale += 0.2 * pm;
-  if (r == 360) {
+    deg ++;
+    r += 1 * pm;
+    scale += 0.2 * pm;
+    if (r == 360) {
     pm = -1;
-  }
-  else if (r == 0) {
+    }
+    else if (r == 0) {
     pm = 1;
-  }
+    }
 }
